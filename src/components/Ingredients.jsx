edd85739aba892f98669800c64d2abfdde3ae108
Evelinach11 <evelinagood2345@gmail.com>
2023-10-31 13:37:06 +0200

    implemented style for ingredients component

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { fetchIngredients } from "../../db/recipeDBService";
 
 export const Ingredients = () => {
@@ -12,14 +12,45 @@ export const Ingredients = () => {
   }, []);
 
   return (
-    <View>
-      <Text>Ingredients</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Інгредієнти</Text>
       {ingredients.map((ing) => (
-        <View key={ing.id}>
-          <Text>{ing.id}</Text>
-          <Text>{ing.name.charAt(0).toUpperCase() + ing.name.slice(1)}</Text>
+        <View style={styles.ingredientList} key={ing.id}>
+          <Text style={styles.ingredientCount}>{ing.id}.</Text>
+          <Text style={styles.ingredientName}>
+            {ing.name.charAt(0).toUpperCase() + ing.name.slice(1)}
+          </Text>
         </View>
       ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#D0E7D2",
+    height: "100%",
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 600,
+    margin: 8,
+    color: "#183D3D",
+  },
+  ingredientList: {
+    flexDirection: "row",
+    margin: 8,
+  },
+  ingredientCount: {
+    marginRight: 2,
+    fontSize: 22,
+    fontWeight: 500,
+    color: "#183D3D",
+  },
+  ingredientName: {
+    marginRight: 2,
+    fontSize: 22,
+    fontWeight: 400,
+    color: "#183D3D",
+  },
+});
